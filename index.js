6b2f97787808edba4725a3a26a3f92056272bdf9
rebeccanewborn <rjn58@cornell.edu>
2017-10-30 13:47:22 -0400

    lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution here:
+
+function driversWithRevenueOver(drivers, revenue) {
+  function enoughRevenue(obj) {
+    return obj.revenue >= revenue;
+  }
+  return drivers.filter(enoughRevenue);
+}
+
+function driverNamesWithRevenueOver(drivers, revenue) {
+  const objArray = driversWithRevenueOver(drivers, revenue);
+  return objArray.map(function(elt) {
+    return elt.name;
+  });
+}
+
+function exactMatch(drivers, attrObj) {
+  const attrKey = Object.keys(attrObj)[0];
+  function attrMatch(obj) {
+    return obj[attrKey] === attrObj[attrKey];
+  }
+  return drivers.filter(attrMatch);
+}
+
+function exactMatchToList(drivers, attrObj) {
+  const objArray = exactMatch(drivers, attrObj);
+  return objArray.map(function(elt) {
+    return elt.name;
+  });
+}
